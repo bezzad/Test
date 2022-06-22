29e807abaaacffa2369f65ebbe0547125ba0e43d
s.amini <saeedeh.amini3000@gmail.com>
2022-06-22 12:08:22 +0430

    2 middleware + error login  and handling + on url  /salam  path

---
+++ src/middleware/WebApplication1/Program.cs
@@ -0,0 +1,33 @@
+using WebApplication1.middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.UseCustomMiddleware(); 
+app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+app.UseMiddleware2();
+
+app.Run();
---
+++ src/middleware/WebApplication1/middleware/Middleware2.cs
@@ -0,0 +1,49 @@
+﻿namespace WebApplication1.middleware
+{
+    public class Middleware2
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<Middleware2> _logger;
+        public Middleware2(RequestDelegate next, ILogger<Middleware2>  logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            _logger.LogInformation("Invoke middleware2");
+            var response = httpContext.Response;
+            try
+            {
+                var y = httpContext.Request.Path.ToString();
+                if (httpContext.Request.Path.ToString() == "/salam" )
+                {
+                    httpContext.Response.StatusCode = 403;
+                    throw new Exception("403");
+                }
+                else
+                {
+                    await _next.Invoke(httpContext);
+                }
+                _logger.LogInformation("Finish Invoke middleware2");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation("Error in middleware2");
+                response.Redirect(String.Format("/Error", "403", ex.Message));
+            }
+
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.  
+    public static class MiddlewareExtensions
+    {
+
+        public static IApplicationBuilder UseMiddleware2(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<Middleware2>();
+        }
+    }
+}
