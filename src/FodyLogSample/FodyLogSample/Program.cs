98dcdbbe3352d1cc6eff0e73f4df49dabd6f17ec
s.amini <saeedeh.amini3000@gmail.com>
2022-07-30 12:50:56 +0430

    fody sample

---
+++ src/FodyLogSample/FodyLogSample/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FodyLogSample;
+
+var u = new Test("salam");
+u.DisplayTest();
+Console.WriteLine("Hello, World!");
+
---
+++ src/FodyLogSample/FodyLogSample/Test.cs
@@ -0,0 +1,23 @@
+﻿using Anotar.NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FodyLogSample
+{
+    class Test 
+    {
+        string Name;
+        public Test(string name)
+        {
+            Name = name;
+        }
+        public void DisplayTest()
+        {
+            LogTo.Debug(Name);
+        }
+
+    }
+}
