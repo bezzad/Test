286a5b833cf690fbd70b0da238cb209ac2c19dff
s.amini <saeedeh.amini3000@gmail.com>
2022-08-03 09:30:46 +0430

    update after code review with behzad

--- src/LiteDbSample/LiteDbSample/HandleOrder.cs
+++ src/LiteDbSample/LiteDbSample/HandleOrder.cs
@@ -9,48 +9,42 @@ namespace LiteDbSample
 {
     public class HandleOrder //controller 
     {
-        public IGenericRepository<Customer, int> customerRepository;
-        IGenericRepository<Product,int> productRepository;
-        IGenericRepository<Order,int> orderRepository;
         UnitOfWork unitOfWork = new UnitOfWork();
         public HandleOrder()
         {
-            customerRepository = unitOfWork.customerRepository;
-            productRepository = unitOfWork.productRepository;
-            orderRepository = unitOfWork.orderRepository;
         }
         public void InsertNewCustomer(Customer customer)
         {
-            customerRepository.Add(customer);
+            unitOfWork.customerRepository.Add(customer);
         }
         public void InsertNewProduct(Product product)
         {
-            productRepository.Add(product);
+            unitOfWork.productRepository.Add(product);
         }
         public void MakeOrder(Order order)
         {
-            orderRepository.Add(order);
+            unitOfWork.orderRepository.Add(order);
         }
         public void DeleteProduct(int id)
         {
-            productRepository.Delete(id);
+            unitOfWork.productRepository.Delete(id);
             
         }
         public void DeleteCustomer(int id)
         {
-            customerRepository.Delete(id);
+            unitOfWork.customerRepository.Delete(id);
         }
         public Product GetProduct(int id)
         {
-            return productRepository.GetById(id);
+            return unitOfWork.productRepository.GetById(id);
         }
         public Customer GetCustomer(int id)
         {
-            return customerRepository.GetById(id);
+            return unitOfWork.customerRepository.GetById(id);
         }
         public void PrintOrder(int orderid)
         {
-            var orders = orderRepository.Includes();
+            var orders = unitOfWork.orderRepository.Includes();
             foreach(Order order in orders)
             {
                 if(order.OrderId == orderid)
