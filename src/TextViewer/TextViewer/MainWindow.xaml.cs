2e34859f0752c49a67525985f905d812632c6865
saeedeh <saeedeh.amini3000@gmail.com>
2022-12-14 09:35:31 +0330

    textviwer sample

---
+++ src/TextViewer/TextViewer/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.DirectoryServices.ActiveDirectory;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.TextFormatting;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace TextViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private int _renderPageHeight;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public int RenderPageHeight
+        {
+            get
+            {
+                return _renderPageHeight;
+            }
+            set
+            {
+                _renderPageHeight = value;
+                OnPropertyChanged(nameof(RenderPageHeight));
+            }
+        }
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            RenderPageHeight = renderPage.GetHeight();
+        }
+    }
+}
---
+++ src/TextViewer/TextViewer/renderPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TextViewer
+{
+    /// <summary>
+    /// Interaction logic for renderPage.xaml
+    /// </summary>
+    public partial class renderPage : Page
+    {
+        private static int FormattedHeight;
+        public renderPage()
+        {
+            InitializeComponent();
+        }
+        private string ReadFileContent()
+        {
+            return File.ReadAllText("D:\\projects\\textviewer\\TextViewer\\TextViewer\\tv.txt");
+        }
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            var currentText = ReadFileContent();
+            var mainwidth = this.ActualWidth;
+            var formattedText = new FormattedText(
+                currentText,
+                CultureInfo.GetCultureInfo("fa"),
+                FlowDirection.RightToLeft,
+                new Typeface("Calibri"),
+                20,
+                Brushes.Black)
+            {
+                MaxTextWidth = this.ActualWidth - 45,
+            };
+            FormattedHeight =(int) formattedText.Height;
+            var o = this.ActualHeight;
+            drawingContext.DrawText(formattedText, new Point(15, 15));
+        }
+        public static int GetHeight()
+        {
+            return FormattedHeight + 30;
+        }
+    }
+}
