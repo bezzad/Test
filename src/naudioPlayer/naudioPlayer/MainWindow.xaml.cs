d6fcc3a98e67649e127f72aa9812d67b4b895ab0
saeedeh <saeedeh.amini3000@gmail.com>
2022-12-25 12:07:53 +0330

    naudio player sample

---
+++ src/naudioPlayer/naudioPlayer/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NAudio;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace naudioPlayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private NAudio.Wave.BlockAlignReductionStream stream = null;
+
+        private NAudio.Wave.DirectSoundOut output = null;
+
+        private void openFileButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Audio File (*.mp3;*.wav)|*.mp3;*.wav;";
+            
+            if(open.ShowDialog().ToString() != "OK") 
+                return;
+
+            DisposeWave();
+
+            if (open.FileName.EndsWith(".mp3"))
+            {
+                //Pcm : Pulse-code modulation is a method used to digitally represent sampled analog signals.
+                NAudio.Wave.WaveStream pcm = NAudio.Wave.WaveFormatConversionStream.CreatePcmStream(new NAudio.Wave.Mp3FileReader(open.FileName));
+                stream = new NAudio.Wave.BlockAlignReductionStream(pcm);
+            }
+            else if (open.FileName.EndsWith(".wav"))
+            {
+                //Waveform Audio File Format : an audio file format standard, developed by IBM and Microsoft, for storing an audio bitstream on PCs
+                NAudio.Wave.WaveStream pcm = new NAudio.Wave.WaveChannel32(new NAudio.Wave.WaveFileReader(open.FileName));
+                stream = new NAudio.Wave.BlockAlignReductionStream(pcm);
+            }
+            else throw new InvalidOperationException("Not a correct audio file type.");
+
+            output = new NAudio.Wave.DirectSoundOut();
+            output.Init(stream);
+            output.Play();
+
+            pauseButton.IsEnabled = true;
+        }
+
+        private void pauseButton_Click(object sender, EventArgs e)
+        {
+            if (output != null)
+            {
+                if (output.PlaybackState == NAudio.Wave.PlaybackState.Playing) 
+                    output.Pause();
+                else if (output.PlaybackState == NAudio.Wave.PlaybackState.Paused) 
+                    output.Play();
+            }
+        }
+
+        private void DisposeWave()
+        {
+            if (output != null)
+            {
+                if (output.PlaybackState == NAudio.Wave.PlaybackState.Playing) 
+                    output.Stop();
+                output.Dispose();
+                output = null;
+            }
+            if (stream != null)
+            {
+                stream.Dispose();
+                stream = null;
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            DisposeWave();
+        }
+    }
+}
